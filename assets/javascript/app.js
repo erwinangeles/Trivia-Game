041a83b34a441cd6245f63e0f638378e3d68f9cd
Erwin Angeles <erwin@eang.media>
2019-10-03 08:51:14 -0500

    added validation notifications, and changed game model to 30 seconds total

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,13 @@
 let currentQuestion = 0;
+let correct = 0;
+let gameoverMSG = "That's it! Check Out Your Score Below";
+let gameoverIMG = "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif";
+let isgameover = false;
+
+
+//status-msg-divs
+let wrongAnswerDiv = `<div class="alert alert-danger" role="alert" style="display: none" id="status-msg-wrong"> Sorry, that's not the right answer. </div>`
+let correctAnswerDiv = `<div class="alert alert-success" role="alert" style="display: none" id="status-msg-correct"> That's right! </div>`
 
 const TriviaQuestions = {
     0: {
@@ -42,14 +51,14 @@ const TriviaQuestions = {
         correct: 2
     },
     8: {
-        prompt: "What is Michael's online username for the online dating website?",
-        answers: ["ReadyForMarriage", "KidCrazy", "LittleKidLover", "Looking4Woman"],
-        correct: 2
-    },
-    9: {
         prompt: "What type of car does Dwight drive?",
         answers: ["Corvette", "Pontiac Trans Am", "Camaro", "Toyota Prius"],
         correct: 1
+    },
+    9: {
+        prompt: "What is Michael's online username for the online dating website?",
+        answers: ["ReadyForMarriage", "KidCrazy", "LittleKidLover", "Looking4Woman"],
+        correct: 2
     }
     
 }
@@ -58,12 +67,13 @@ console.log(TriviaQuestions)
 
 
 function setQAButtons(){
-
-
-    //sets the question in the h2 tag
+    if(currentQuestion > 9){
+        gameOver();
+    }else{
+        //sets the question in the h2 tag
     $("#question").html(`<h2>${TriviaQuestions[currentQuestion].prompt}</h2>`);
-    $("#timer").text(`30`);
-    n = 30;
+    // $("#timer").text(`30`);
+    // n = 30;
 
     //loads the answers
     for(let i = 0; i < 4; i++){
@@ -79,20 +89,30 @@ function setQAButtons(){
     $(".btn").on("click", function(){
         if($(this).data("answer") == TriviaQuestions[currentQuestion].correct){ //checks if answer is correct
             // $(this).removeClass("btn-outline-secondary"); //removes default outline
-            $(this).addClass("correct") //adds green outline
+            // $(this).addClass("correct") //adds green outline
+            $("#status-msg-wrong").hide();
+            $("#status-msg-correct").show();
+            setTimeout(function(){ $("#status-msg-correct").hide("slow"); }, 2000)
+            correct++
+
             currentQuestion++
             $("#answers").empty(); //clears answers div
             $("#question").empty(); //clears question div
-            if(currentQuestion == 9){
-             gameOver();
-            }else{
-                setQAButtons();
-            }
+            setQAButtons();
         }else{
-            $(this).addClass("wrong") //adds red outline
+            // $(this).addClass("wrong") //adds red outline
+            $("#status-msg-correct").hide();
+            $("#status-msg-wrong").show();
+            setTimeout(function(){ $("#status-msg-wrong").hide("slow"); }, 2000)
+            //skip to next question
+            currentQuestion++
+            $("#answers").empty(); //clears answers div
+            $("#question").empty(); //clears question div
+            setQAButtons();
         }
 
     })
+    }
 }
 
 
@@ -104,16 +124,21 @@ function setQAButtons(){
      if(n > 0){
         setTimeout(countDown,1000);
         $('#timer').text(n)
-     }else{
-         gameOver();
+     }else if(n == 0 && isgameover == false){
+        gameoverMSG = "Times up! Game over";
+        gameoverIMG = "https://media.giphy.com/media/KhliiAkDFP9YY/giphy.gif";
+        gameOver();
      }
   }
 
   function gameOver(){
-    $("#question").html("<h2>Game Over!</h2>")
+    isgameover = true;
+    $("#question").html(`<h2>${gameoverMSG}</h2>`)
     let giphy = $("<img>")
-    giphy.attr("src", "https://media.giphy.com/media/KhliiAkDFP9YY/giphy.gif")
-    $("#answers").html(giphy);
+    giphy.attr("src", `${gameoverIMG}`)
+    let correctheader = `<h4>Correct Answers: <span style="color:green">${correct}</span> out of 10</h4>`
+    $("#answers").html(correctheader)
+    $("#answers").append(giphy);
 
     let button = $("<a>");
     $(button).html(`<br><br><a class="btn btn-primary" href="javascript:location.reload();" role="button">Play Again</a>`)
